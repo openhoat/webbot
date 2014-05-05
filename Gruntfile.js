e0af68ed03cbb6200a141490ced969f753cb949e
Olivier Penhoat <openhoat@gmail.com>
2014-05-05 18:36:47 +0200

    debug cmd

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
       options: {
         reporter: require('jshint-stylish'),
         force: true,
-        jshintrc: 'jshint.json',
+        jshintrc: 'jshint.json'
       },
       src: [
         'lib/**/*.js',
