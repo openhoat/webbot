5730beb6429d2deb1478c83ceda7706febeb039a
Olivier Penhoat <openhoat@gmail.com>
2014-12-27 17:46:57 +0100

    doc

--- README.md
+++ README.md
@@ -37,6 +37,34 @@ Example of directory structure :
 - [config](https://github.com/openhoat/webbot/tree/master/config/scenarii)
 - [scenarii](https://github.com/openhoat/webbot/tree/master/scenarii)
 
+### Create a web test script : scenarii/mywebtest.js
+
+    var chai = require('chai')
+      , webbot = require('webbot')
+      , expect = chai.expect
+      , browser, scenario;
+
+    browser = webbot.getBrowser();
+    scenario = webbot.getScenario();
+
+    describe('Simple web test', function () {
+      after(function (done) {
+        browser
+          .end(done);
+      });
+      it('should check web page title', function (done) {
+        browser
+          .log('Connect to %s', scenario.config.url)
+          .url(scenario.config.url)
+          .log('Check page title : %s', scenario.config.title)
+          .getTitle(function (err, value) {
+            expect(err).to.be.undefined;
+            expect(value).to.equal(scenario.config.title);
+          })
+          .call(done);
+      });
+    });
+
 ### Play WebBot :
 
     # webbot -e live -s mywebtest
--- scenarii/mywebtest.js
+++ scenarii/mywebtest.js
@@ -1,30 +1,24 @@
 var chai = require('chai')
   , webbot = require('../lib/webbot')
   , expect = chai.expect
-  , browser, config;
+  , browser, scenario;
+
+browser = webbot.getBrowser();
+scenario = webbot.getScenario();
 
 describe('Simple web test', function () {
-
-  before(function (done) {
-    browser = webbot.getBrowser();
-    config = {};
-    config.scenario = webbot.getScenario().config;
-    webbot.addBrowserCommands(browser, done);
-  });
-
   after(function (done) {
     browser
       .end(done);
   });
-
-  it('should home page', function (done) {
+  it('should check web page title', function (done) {
     browser
-      .log('Connect to %s', config.scenario.url)
-      .url(config.scenario.url)
-      .log('Check page title : %s', config.scenario.title)
+      .log('Connect to %s', scenario.config.url)
+      .url(scenario.config.url)
+      .log('Check page title : %s', scenario.config.title)
       .getTitle(function (err, value) {
         expect(err).to.be.undefined;
-        expect(value).to.equal(config.scenario.title);
+        expect(value).to.equal(scenario.config.title);
       })
       .call(done);
   });
