584d66fa47f7b50527209b32dfa5b0f3f508fc18
Olivier Penhoat <openhoat@gmail.com>
2014-12-27 12:15:25 +0100

    debug build

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function (grunt) {
   if (!process.env.DEBUG) {
     process.env.DEBUG = '*:info|warn|error';
   }
-  if (grunt.option('ci')) {
+  if (process.env.JENKINS_URL) {
     gruntConfig.jshint.options.reporter = 'checkstyle';
     gruntConfig.jshint.options.reporterOutput = 'dist/reports/jshint_checkstyle.xml';
     gruntConfig.mochaTest.test.options.reporter = 'xunit-file';
